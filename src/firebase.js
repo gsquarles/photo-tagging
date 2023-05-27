6b9633d63258a43d5569bd5dfbc7834c80bbe569
Griffin Quarles <griffinquarles11@gmail.com>
2023-05-27 15:56:02 -0500

    setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwi0rX8Y7psQU8qjBAR0erTx1ySPFwa48",
+  authDomain: "photo-tagging-c233a.firebaseapp.com",
+  projectId: "photo-tagging-c233a",
+  storageBucket: "photo-tagging-c233a.appspot.com",
+  messagingSenderId: "820737538665",
+  appId: "1:820737538665:web:ddceeabf9a5e7b5b0040f8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
