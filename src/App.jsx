ff9d8d99b27bf9ffb0fdb9b8023dcd4a17c89f1b
Griffin Quarles <griffinquarles11@gmail.com>
2023-05-29 12:04:02 -0500

    Level Select Page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { LevelSelectPage } from "./components/LevelSelectPage";
 
 function App() {
   return (
     <>
       <Header />
+      <LevelSelectPage />
     </>
   );
 }
---
+++ src/components/LevelSelectPage.jsx
@@ -0,0 +1,47 @@
+import beachImg from "../imgs/beachLarge.jpg";
+import stadiumImg from "../imgs/stadiumLarge.jpg";
+import spaceImg from "../imgs/spaceLarge.jpg";
+
+const LEVELS = [
+  { title: "beach", image: beachImg },
+  { title: "stadium", image: stadiumImg },
+  { title: "space", image: spaceImg },
+];
+
+export function LevelSelectPage() {
+  return (
+    <main className='flex py-10 px-28'>
+      <div className='w-1/3'>
+        <h1 className='text-4xl font-bold mb-5'>
+          It's all about finding the characters!
+        </h1>
+        <p className='text-xl mb-5'>
+          After you select a level, click on the screen and choose a character.
+          If you are right about their location. A marker will be placed there.
+          You win when you find all the characters.
+        </p>
+        <p className='text-xl'>
+          If you qualify, your score will be recorded in the high score table.
+        </p>
+      </div>
+      <div className='w-2/3  flex flex-col items-center'>
+        {LEVELS.map((level, index) => (
+          <div
+            key={index}
+            id={level.title}
+            className='w-[85%] p-12 rounded-lg opacity-70 hover:opacity-100 mb-6'
+            style={{
+              backgroundImage: `url(${level.image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <h1 className=' text-primary text-6xl font-bold capitalize'>
+              {level.title}
+            </h1>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
